db32c16a55b35c3f09d187710e070fb5c0928b8a
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-14 21:08:32 +0530

    Added: present and Absent

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,9 +1,16 @@
 package empWageComputation;
 
-public class EmpWageWelcome {
-
+public class EmpWageComputationUC1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("Welcome to EmpWageComputation");
+		int IS_FULL_TIME = 1;
+		//Compitation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
 	}
+}
 }
