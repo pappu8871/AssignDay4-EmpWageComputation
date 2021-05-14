9eba271bc1f05f2edd0cc0c706e55392b2e82cdf
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-14 21:21:48 +0530

    Added:class EmpWageComputationUC2.java

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,16 +1,19 @@
 package empWageComputation;
 
-public class EmpWageComputationUC1 {
+public class EmpWageComputationUC2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 
 		int IS_FULL_TIME = 1;
-		//Compitation
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		
-		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is present");
-		} else {
-			System.out.println("Employee is absent");
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 1;
+		int empWage = 0;
+		double empCheck = Math.floor( Math.random() * 10 ) % 2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println(" Emp Wage: " + empWage);
 	}
-}
 }
