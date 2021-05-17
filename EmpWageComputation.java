084a3d86da4a90be32edef72771e4803e92ab334
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-17 18:44:16 +0530

    Added:EmpWageComputationUC8.java used methor and function Argument

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,44 +1,57 @@
 package empWageComputation;
 
-public class EmpWageComputationUC7 {
+public class EmpWageComputationUC8 {
 
-	    public static final	int IS_PART_TIME=1;
-		public static final int IS_FULL_TIME=2;
-		public static final	int EMP_RATE_PER_HOUR = 20;
-		public static final int NUM_OF_WORKING_DAYS = 20;
-	    public static final int MAX_HRS_IN_MONTH = 100;
-		
-		public static int empWage() {
+	public static final	int IS_PART_TIME=2;
+	public static final int IS_FULL_TIME=1;
+	
 
-			    int empHrs = 0;
-				int totalEmpHrs = 0;
-			    int totalWorkingDays = 0;
-				
-				while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
-					  totalWorkingDays < NUM_OF_WORKING_DAYS) {
-					  totalWorkingDays++;
-				int empCheck =(int) Math.floor( Math.random() * 10 ) % 3;
-				switch (empCheck) {
-				case IS_PART_TIME:
-					empHrs =4;
-					break;
-				case IS_FULL_TIME:
-					empHrs = 8;
-					break;
-					default:
-				    empHrs =0;
+static void calculateTotalwage(String company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, 
+		int MAX_HRS_IN_MONTH) {
+	
+	     int partTimeEmpHr=4;
+	    int fullTimeEmpHrs = 8;
+    	int empWage=0;
+        int totalMonthlyWage=0;
+	    int totalWorkingHrs=0;
+	    int totalWorkingDays=0;
+	   
+    	while (totalWorkingDays < NUM_OF_WORKING_DAYS && totalWorkingHrs < MAX_HRS_IN_MONTH) {
+        		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        			switch (empCheck) {
+
+            			case 0:
+                				System.out.println("Employee is absent");
+                				break;
+
+            			case 1:
+                				System.out.println("Employee is Present");
+                				empWage = Math.multiplyExact(fullTimeEmpHrs, EMP_RATE_PER_HOUR);
+                				System.out.println("Employee Daily  Wage is :" + empWage);
+                				totalWorkingHrs = totalWorkingHrs + 8;
+                				totalWorkingDays = totalWorkingDays++;
+                				break;
+            			case 2:
+                				System.out.println("Employee is Present but Half-Time");
+                   			empWage = Math.multiplyExact(partTimeEmpHr, EMP_RATE_PER_HOUR);
+    				        System.out.println("Employee's Part-Time Wage is :" + empWage);
+                				totalWorkingHrs = totalWorkingHrs + 4;
+                				totalWorkingDays = totalWorkingDays++;
+                				break;
+        				}
+        				totalMonthlyWage = totalMonthlyWage + empWage;
+    			}
+    
+    			System.out.println("Company name: "+company);
+    			System.out.println("Employee Monthly Wage is :" + totalMonthlyWage);
+    
 			}
-				totalEmpHrs += empHrs;
-				System.out.println("Days#: " + totalWorkingDays + " Emp Hrs:" +empHrs );
-				}
-				int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-				System.out.println("Total Emp Wage: " + totalEmpWage);
-		         return totalEmpWage;
-		}
-				public static void main(String[] args) {
-					empWage();
-					
-		}
+
+			public static void main(String [] args) {
+   			System.out.println("Welcome To Employee wage Computation Program");
+		calculateTotalwage("Flipcart", 20, 25, 100);
+		calculateTotalwage("Bigbasket", 30, 15, 50);
 	}
-		
+
+}
 
